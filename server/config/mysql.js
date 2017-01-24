a535ccee585a19c3cff9a1759129596ed79be6e9
Chris Rollins <chris.rollins@leapfire.com>
2017-01-23 17:26:17 -0800

    still fixing sql

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -74,8 +74,7 @@ function tryDestroy(connection){
 	}
 }
 
-exps.query("set global sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"
-, function(err, result){
+exps.query("set global sql_mode = ''", function(err, result){
 	// console.log(err, result);
 });
 
